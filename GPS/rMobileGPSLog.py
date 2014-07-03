7b7931d57d24c33f2f16e96c5bde6a8f34dee63d
Jon Tungland <jon@tungland.org>
2014-07-04 00:18:25 +0200

    Initial commit

---
+++ GPS/rMobileGPSLog.py
@@ -0,0 +1,78 @@
+import gps
+import os
+import MySQLdb
+import sqlite3
+from math import radians, cos, sin, asin, sqrt
+
+# grabbed from http://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points (Michael Dunn)
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+
+    # 6367 km is the radius of the Earth
+    km = 6367 * c
+    return km 
+ 
+# Listen on port 2947 (gpsd) of localhost
+session = gps.gps("localhost", "2947")
+session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+
+#db = MySQLdb.connect("172.20.100.2","gps","b4y4vy4tbyrty","gps")
+#cursor = db.cursor()
+db = sqlite3.connect('/var/tmp/gps.db',10)
+cursor = db.cursor()
+
+createdb = 'create table if not exists gpslog(time TEXT, speed TEXT, lat TEXT, lon TEXT, alt TEXT, extra TEXT, time2 TEXT, epv TEXT, ept TEXT, track TEXT, climb TEXT, distance TEXT);'
+cursor.execute(createdb)
+db.commit()
+
+attribs = ['time', 'speed', 'alt', 'lat', 'lon', 'epv', 'ept', 'track', 'climb']
+while True:
+	try:
+		report = session.next()
+		if report['class'] == 'TPV' and hasattr(report, 'lat') and hasattr(report,'lon'):
+			print '+-----------------------------+'
+			distance=9999999;
+			if 'prevreport' in locals():
+				distance = haversine(report.lon, report.lat, prevreport.lon, prevreport.lat) * 1000
+				print "Moved ", distance, " m"
+				print " from " , prevreport.lon ,",", prevreport.lat
+				print " to " , report.lon ,",", report.lat
+			if distance < 5:
+				continue;
+			sqlnames = "time2,distance"
+			sqlvalues = "datetime(),'" + str(distance) + "'"
+			
+			for attrib in attribs:
+				if hasattr(report, attrib):
+					sqlnames += ',' + attrib
+					sqlvalues += ",'" + str(report[attrib])  + "'"
+					print attrib + ': ', report[attrib]
+
+			print '+-----------------------------+'
+			sql = "INSERT INTO gpslog (" + sqlnames + ") VALUES (" + sqlvalues + ")"
+			
+			print sql
+			
+			cursor.execute(sql)
+			db.commit()
+			#print report
+			prevreport = report;
+	except KeyError:
+		pass
+	except KeyboardInterrupt:
+		quit()
+	except StopIteration:
+		session = None
+		print "GPSD has terminated"
---
+++ GPS/rMobileGPSUploader.php
@@ -0,0 +1,97 @@
+<?php
+
+function ping($host, $port, $timeout) { 
+  $tB = microtime(true); 
+  $fP = @fSockOpen($host, $port, $errno, $errstr, $timeout); 
+  if (!$fP) { return false; } 
+  $tA = microtime(true); 
+  return round((($tA - $tB) * 1000), 0)." ms"; 
+}
+
+$accepted_ssids = array("tungland");
+while(1){
+	$ssid = exec("iwgetid -r");
+	echo "connected to SSID={$ssid}\n";
+	if(!in_array($ssid, $accepted_ssids)){
+		echo "Not connected to a known SSID, sleeping\n";
+		sleep(60);
+		continue;
+	}
+
+	$r = ping("172.20.100.2","22","1000");
+	if($r === false){
+		echo "... No internet/LAN access, sleeping 10 sec\n";
+		sleep(60);
+		continue;
+	}
+	$r = connectAndSyncDB();
+	if($r == 100){
+		echo "Sleeping 1 sec since we have more rows in queue ... \n";
+		sleep(1);
+	
+	}else{
+		echo "Sleeping 60 sec ... \n";
+		sleep(10);
+	}
+	continue;
+}
+
+function connectAndSyncDB() {
+	echo "Trying to sync db ... \n";
+	$localdb = new SQLite3("/var/tmp/gps.db");
+	if(!$localdb){
+		echo "Could not connect to local db... breaking.\n";
+		return;
+	}
+	$remotedb = new MySQLi("172.20.100.2","gps","b4y4vy4tbyrty","gps");
+	if(!$remotedb){
+		echo "Could not connect to remote db... breaking.\n";
+		$localdb->close();
+		return;
+	}
+	$localdb->busyTimeout(10000);
+
+	echo "Syncing...\n";
+	$num = syncDB($localdb, $remotedb);
+	echo "Synced {$num} rows \n";
+	$localdb->close();
+	$remotedb->close();
+	return $num;
+}
+
+function syncDB($localdb, $remotedb){
+	$query = "SELECT rowid, * FROM gpslog ORDER BY time2 LIMIT 100";
+	$result = $localdb->query($query);
+	if(!isset($result)){
+		echo "Not possible to get local records, breaking\n";
+		return -1;
+	}
+	$count=0;
+	while($rowf = $result->fetchArray(SQLITE3_ASSOC)){
+		$query = "INSERT INTO gps.gpslog (
+			time,time2,lat,lon,speed,alt,extra,epv,ept,track,climb,distance
+			) VALUES (
+			'{$rowf['time']}','{$rowf['time2']}','{$rowf['lat']}','{$rowf['lon']}','{$rowf['speed']}','{$rowf['alt']}','{$rowf['extra']}','{$rowf['epv']}','{$rowf['ept']}','{$rowf['track']}','{$rowf['climb']}','{$rowf['distance']}'
+			)";
+		if($remotedb->query($query)){
+			$count++;
+			$last=$rowf['rowid'];
+		}else{
+			echo "Failed to insert on remote server, skipping.\n";
+			break;
+		}
+		//if($count%50==0){
+		//	echo "Waiting 1 sec for other processes\n";
+		//	sleep(2);
+		//}
+	}
+	if(isset($last)){
+		$query = "DELETE FROM gpslog WHERE rowid<='{$last}'";
+		$localdb->exec($query);
+		echo "Deleting: {$query}\n";
+	}
+	return $count;
+
+}
+
+?>
