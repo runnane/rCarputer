d85073191fe5ff2d7645c3e09ccf79fa97fb4718
Jon Tungland <jon@tungland.org>
2014-07-08 23:15:55 +0200

    added missing settings

--- GPS/settings.php
+++ GPS/settings.php
@@ -1,13 +1,13 @@
 <?php
 
-$accepted_ssids = array("tungland", "fwg", "Telenor5944fus", "supah");
+$accepted_ssids = array("linksys");
 $nics = array("eth0","wlan0");
 
-define("REST_USERNAME","rpi2");
-define("REST_PASSWORD", "oiuhnoiuabsigonygosgndoiagy");
-define("REST_ENDPOINT", "https://projects.runnane.no/gps/rest.php");
+define("REST_USERNAME","");
+define("REST_PASSWORD", "");
+define("REST_ENDPOINT", "");
 
-define("LATENCY_TARGET", "81.29.32.130");
+define("LATENCY_TARGET", "8.8.8.8");
 
 define("REPORTIN_INTERVAL", 900); // every 15 min
 define("SQLITE_FILE", "/ssd/db/rcarputer.db");
